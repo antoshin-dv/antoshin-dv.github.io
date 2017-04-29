c253d252165714b0de69479f1e3d6b2bb5e2ec70
antoshin-dv <antoshin.dv@gmail.com>
2017-04-30 02:44:42 +0300

    Генерация песни "99 бутылок пива" при помощи JS

---
+++ 99_bottles_of_beer/code.js
@@ -0,0 +1,17 @@
+﻿var word="бутылок";
+var count = 99;
+while (count > 0)
+{
+	document.write(count + " " + word + " пива на стене<br>");
+	document.write(count + " " + word + " пива!<br>");
+	document.write("<b>Возьми одну</b>, пусти по кругу<br>");
+	count = count - 1;
+	if (count > 0)
+	{
+		document.write(count + " " + word + " пива на стене!<br>");
+	}
+	else
+	{
+		document.write("Нет больше " + word + " пива на стене!<br>")
+	}
+}
