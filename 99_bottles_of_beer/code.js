853d9c3feda2b1ae8b49de6a764303f94a0a31c3
antoshin-dv <antoshin.dv@gmail.com>
2017-04-30 21:32:35 +0300

    Поправлен вывод бутылок с учётом склонения

--- 99_bottles_of_beer/code.js
+++ 99_bottles_of_beer/code.js
@@ -1,17 +1,30 @@
-﻿var word="бутылок";
-var count = 99;
+﻿var count = 99;
 while (count > 0)
 {
-	document.write(count + " " + word + " пива на стене<br>");
-	document.write(count + " " + word + " пива!<br>");
+	document.write(count + " " + GetButtles(count) + " пива на стене<br>");
+	document.write(count + " " + GetButtles(count) + " пива!<br>");
 	document.write("<b>Возьми одну</b>, пусти по кругу<br>");
 	count = count - 1;
 	if (count > 0)
 	{
-		document.write(count + " " + word + " пива на стене!<br>");
+		document.write(count + " " + GetButtles(count) + " пива на стене!<br>");
 	}
 	else
 	{
-		document.write("Нет больше " + word + " пива на стене!<br>")
+		document.write("Нет больше " + GetButtles(count) + " пива на стене!<br>")
 	}
+}
+
+function GetButtles(count)
+{
+	var word;
+	var mod = count % 10;
+	if (mod == 0 || mod >= 5 || (count >= 11 && count <= 14))
+		word = "бутылок";
+	else if (mod >= 2)
+		word = "бутылки";
+	else
+		word = "бутылка";
+	
+	return "<i>" + word + "</i>";
 }
