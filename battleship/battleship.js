53478d103200b18e4999f14cb78187d46f553479
antoshin-dv <antoshin.dv@gmail.com>
2017-05-03 03:08:22 +0300

    Добавлена модель

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -1,4 +1,5 @@
-﻿var view = 
+﻿// Представление
+var view = 
 {
 	displayMessage: function(msg)
 	{
@@ -20,13 +21,54 @@
 	}
 };
 
-view.displayMessage("Да начнётся тестирование.");
-var userFire = ["A0", "D4", "F5", "B2", "C5", "C6"];
-var isHit = [false, true, false, true, false, true];
-for (var i = 0; i < userFire.length; i++)
+// Модель
+var model =
 {
-	if (isHit[i])
-		view.displayHit(userFire[i]);
-	else
-		view.displayMiss(userFire[i]);
-}
+	boardSize: 7,
+	numShips: 3,
+	shipLength: 3,
+	shipsSunk: 0,
+	ships: 
+	[
+		{ locations: ["B0", "C0", "D0"], hits: [false, false, false] },
+		{ locations: ["D2", "D3", "D4"], hits: [false, false, false] },
+		{ locations: ["G3", "G4", "G5"], hits: [false, false, false] }
+	],
+	
+	fire: function(location)
+	{
+		for (var i = 0; i < this.ships.length; i++)
+		{
+			var ship = this.ships[i];
+			var idx = ship.locations.indexOf(location);
+			if (idx >= 0)
+			{
+				ship.hits[idx] = true;
+				view.displayHit(location);
+				if (this.isSunk(ship))
+				{
+					this.shipsSunk++;
+					view.displayMessage("Убил");
+				}
+				else
+					view.displayMessage("Ранил");
+				return true;
+			}
+		}
+		view.displayMiss(location);
+		view.displayMessage("Мимо");
+		return false;
+	},
+	
+	isSunk: function(ship)
+	{
+		for (var i = 0; i < ship.hits.length; i++)
+			if (!ship.hits[i])
+				return false;
+		return true;
+	}
+};
+
+var userFire = ["A0", "D4", "F5", "B2", "C5", "C6", "C0", "D0"];
+for (var i = 0; i < userFire.length; i++)
+	model.fire(userFire[i]);
